9370ca020559f5f679e7acff9cbba402ac883bce
GIordano <giordano.daloisio@gmail.com>
2022-12-05 17:57:03 +0100

    add docs

--- metrics.py
+++ metrics.py
@@ -24,14 +24,31 @@ def __compute_probs(data_pred, label_name, positive_label, group_condition):
     return unpriv_group_prob, priv_group_prob
 
 
-def disparate_impact(data_pred, group_condition, label_name, positive_label):
+def disparate_impact(data_pred: pd.DataFrame, group_condition: dict, pred_label_name: str, positive_label: int):
+    
+    '''
+    data_pred: Dataframe with true and predicted values
+    group_condition: Dictionary where the keys are the sensitive variables and the values are the values of the unprivileged group
+    pred_label_name: Name of the predicted label
+    positive_label: Positive value of the label
+    '''
+
     unpriv_group_prob, priv_group_prob = __compute_probs(
-        data_pred, label_name, positive_label, group_condition)
+        data_pred, pred_label_name, positive_label, group_condition)
     return min(unpriv_group_prob / priv_group_prob,
                priv_group_prob / unpriv_group_prob) if unpriv_group_prob != 0 and priv_group_prob != 0 else 0
 
-def accuracy(df_pred: pd.DataFrame, label: str):
-    return accuracy_score(df_pred['y_true'].values, df_pred[label].values)
+def accuracy(df_pred: pd.DataFrame, true_label: str, pred_label: str):
+    '''
+    data_pred: Dataframe with true and predicted values
+    true_label: Name of the true label
+    pred_label: Name of the predicted label
+    '''
+
+    return accuracy_score(df_pred[true_label].values, df_pred[pred_label].values)
 
 def hmean(metrics: list):
+    '''
+    metrics: List of metrics values
+    '''
     stats.hmean(metrics)
